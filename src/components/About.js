25c66b84618d53e7731711e3f31889d9291e258b
nrooney99 <84242801+nrooney99@users.noreply.github.com>
2022-05-05 17:06:26 -0300

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export default function About() {
     return(
         <div className="about">
         <h3>About</h3>
-        <p>I am an Industrial Engineering student who is always looking for new things to learn. I know Python, Java and Javascript, but I want to go beyond.</p>
+        <p>I am an Industrial Engineering student THAT IS IN THE CONSTANT SEARCH FOR NEW KNOWLEDGE. I know Python, Java and Javascript, but I want to go beyond.</p>
         </div>
     )
 }
