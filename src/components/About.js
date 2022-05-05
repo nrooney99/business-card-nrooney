84fce8d53657c677e867e2ae810027725099d115
nrooney99 <84242801+nrooney99@users.noreply.github.com>
2022-05-05 17:23:33 -0300

    Typo

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export default function About() {
     return(
         <div className="about">
         <h3>About</h3>
-        <p>I am an Industrial Engineering student looking for new challenges. I love programming and learning new technologies. Actually developing with Python and Javascript.</p>
+        <p>I am an Industrial Engineering student looking for new challenges. I love programming and learning new technologies. Currently developing with Python and Javascript.</p>
         </div>
     )
 }
